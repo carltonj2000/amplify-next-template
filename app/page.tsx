dde69acf1bdf00acc51fa13d8682cce8a8affd55
Carlton Joseph <carlton.joseph@gmail.com>
2025-02-17 18:45:37 -0800

    update userid display

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function App() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user?.username}</h1>
+          <h1>Hello {user?.signInDetails?.loginId}</h1>
           <button onClick={createTodo}>+ new</button>
           <ul>
             {todos.map((todo) => (
